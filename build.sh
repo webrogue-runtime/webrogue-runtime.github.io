29b3fced040f589a9690a8222059d0dd963619eb
Artem Borovik <polzovatellllk@gmail.com>
2025-05-31 20:06:43 +0300

    404

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ cd "$(dirname $0)"
 set -ex
 
 WEBROGUE_DOC_IS_EXPORT=true npx next build
-rm out/**/*.txt
+rm out/**/*.txt out/*.txt
---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import { MarkdownPage, MarkdownPageMetadata } from "@/components/markdown_page";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: "Page not found",
+        icons: "/favicon.ico",
+    }
+}
+
+export default async function NotFoundPage() {
+    return <div className="notFoundContainer">
+        <h1 className="notFound">404 :(</h1>
+        <h2 className="notFound">Requested page not found</h2>
+    </div>;
+}
